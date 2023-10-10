934a472c4d0af68f4ba45bd2834338fafcc278bf
Charles Demers <charles.demers6@gmail.com>
2023-10-10 09:32:53 -0400

    Refresh queries when the app gains focus and when changing the language

--- template/src/navigation/index.tsx
+++ template/src/navigation/index.tsx
@@ -4,10 +4,17 @@ import {
   DefaultTheme,
   DarkTheme,
 } from '@react-navigation/native';
+import { AppState } from 'react-native';
+import { focusManager } from '@tanstack/react-query';
 import RootNavigator from './RootNavigator';
 import { withBoundary } from '~/components/boundary';
 import { useColorSchemeValue } from '~/hooks/use-color-scheme-value';
 import { useKillswitch } from '~/hooks/use-killswitch';
+
+// Notify react-query when the focused state changes
+AppState.addEventListener('change', (appState) => {
+  focusManager.setFocused(appState === 'active');
+});
 
 function Navigation() {
   const theme = useColorSchemeValue([DefaultTheme, DarkTheme]);
