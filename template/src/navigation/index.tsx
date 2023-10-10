23d1451906031d9ccbc28138116342a692b38c08
Charles Demers <charles.demers6@gmail.com>
2023-10-10 14:49:47 -0400

    Wrap subscription in effect and add onlineManager

--- template/src/navigation/index.tsx
+++ template/src/navigation/index.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
 } from '@react-navigation/native';
 import { AppState } from 'react-native';
-import { focusManager } from '@tanstack/react-query';
+import { focusManager, onlineManager } from '@tanstack/react-query';
+import NetInfo from '@react-native-community/netinfo';
 import RootNavigator from './RootNavigator';
 import { withBoundary } from '~/components/boundary';
 import { useColorSchemeValue } from '~/hooks/use-color-scheme-value';
 import { useKillswitch } from '~/hooks/use-killswitch';
 
-// Notify react-query when the focused state changes
-AppState.addEventListener('change', (appState) => {
-  focusManager.setFocused(appState === 'active');
-});
-
 function Navigation() {
   const theme = useColorSchemeValue([DefaultTheme, DarkTheme]);
 
   useKillswitch();
+
+  useEffect(() => {
+    // Refetch queries on app focus
+    const subscription = AppState.addEventListener('change', (appState) => {
+      focusManager.setFocused(appState === 'active');
+    });
+
+    return () => subscription.remove();
+  }, []);
+
+  useEffect(() => {
+    // Refetch queries on reconnect
+    return NetInfo.addEventListener((state) => {
+      onlineManager.setOnline(!!state.isConnected);
+    });
+  }, []);
 
   return (
     <NavigationContainer
