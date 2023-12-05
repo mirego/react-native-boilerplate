6fb3f2d415c4c5b53a0b67240ae332cec817320b
Charles Demers <charles.demers6@gmail.com>
2023-12-05 15:07:04 -0500

    Update dependencies

--- template/src/services/query-client.ts
+++ template/src/services/query-client.ts
@@ -1,9 +1,3 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export default new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    },
-  },
-});
+export default new QueryClient();
